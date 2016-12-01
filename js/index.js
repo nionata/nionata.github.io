24d886366943cf958d119dfd152fd0bc97324c26
Nicholas Ionata <n.ionata129@gmail.com>
2016-12-01 16:37:17 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll != 0) {
+    $("#nav").addClass("top-bar");
+    $("#social").removeClass("social-tran");
+  } else {
+    $("#nav").removeClass("top-bar");
+    $("#social").addClass("social-tran");
+  }
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
