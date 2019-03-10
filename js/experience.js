e3ca57670506a45b3fae6dfadb20bd13355f2bd9
Nicholas Ionata <n.ionata129@gmail.com>
2019-03-09 22:08:12 -0500

    Finished implementing experience template

--- js/experience.js
+++ js/experience.js
@@ -1,18 +1,53 @@
 var experience = [
   {
-    name: "",
-    position: "",
+    name: "Raymond James Financial",
+    position: "Software Developer Intern",
     date: {
-      start: "",
-      end: ""
+      start: "May 2018",
+      end: "August 2018"
     },
-    description: "",
-    technologies: [],
+    description: "I worked on Enterprise Services and the Secure Software Development Lifecycle.",
+    technologies: ["Java", "Spring", "OracleDB"],
     links: [
       {
-        name: "",
-        link: ""
+        type: "Website",
+        link: "https://www.raymondjames.com/"
       }
-    ]
+    ],
+    image: "http://www.raymondjames.com/freestone/images/aboutus_banner.png"
+  },
+  {
+    name: "Scanned",
+    position: "Co-Founder/Software Developer",
+    date: {
+      start: "September 2016",
+      end: "September 2018"
+    },
+    description: "A startup I co-founded and worked as the tech lead. Through mobile and web apps we provided a highly efficient group attendance tracking service.",
+    technologies: ["React.js", "React Native", "Firebase"],
+    links: [
+      {
+        type: "Website",
+        link: "/Scanned-Website/"
+      }
+    ],
+    image: "images/scanned.png"
+  },
+  {
+    name: "Lockheed Martin",
+    position: "Industrial Engineering Intern",
+    date: {
+      start: "May 2017",
+      end: "August 2017"
+    },
+    description: "I worked on data analytics and reporting. I created two tools that drastically reduced process times.",
+    technologies: ["VBA", "Excel", "Data Analysis"],
+    links: [
+      {
+        type: "Website",
+        link: "https://www.lockheedmartin.com/us.html"
+      }
+    ],
+    image: "http://aeroengineer.org/wp-content/uploads/2016/11/LOCKHEED.png"
   }
 ]
