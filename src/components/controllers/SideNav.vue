1589c8163ff6726a00fa74758e3faa0326581ad8
nionata <n.ionata129@gmail.com>
2019-07-18 18:00:07 -0400

    side nav overhaul

--- src/components/controllers/SideNav.vue
+++ src/components/controllers/SideNav.vue
@@ -1,58 +1,69 @@
 <template>
-    <div id="sideNav">
-        <div v-for="group in navGroups" v-bind:key="group.title" class="navGroup">
+    <div id="sideNav" :class="{navOpen: mobileNavShow, navClose: !mobileNavShow}">
+        <i class="fas" :class="{'fa-bars': !mobileNavShow, 'fa-times': mobileNavShow}" @click="onMobileNavToggle" id="navToggle"/>
+        <div v-for="group in navGroups" :key="group.title" class="navGroup">
             <div class="navMediaHolder">
-                <i v-bind:class="group.icon"></i>
+                <i class="fas fa-fw" :class="group.icon"></i>
             </div>
             <div class="navContentHolder">
                 <p class="navTitle">{{group.title}}</p>
                 <p 
                     v-for="item in group.items" 
-                    v-bind:key="item" 
-                    v-on:click="$emit('navSelect', item)" 
+                    :key="item" 
+                    @click="onNavItemClick(item)" 
                     class="navItem" 
-                    v-bind:class="{navItemSelected: item === data.modal.display}">{{item}}
+                    :class="{navItemSelected: item === data.modal.display}">{{item}}
                 </p>
             </div>
         </div>
         <div id="navFooter">
-            <div id="linkGroup">
+            <div id="linkGroup" class="footerSection">
                 <a href="https://github.com/nionata" target="_blank"><i class="fab fa-github fa-2x"></i></a>
                 <a href="mailto:nionata@ufl.edu" target="_blank"><i class="far fa-envelope fa-2x"></i></a>
                 <a href="https://www.linkedin.com/in/nionata/" target="_blank"><i class="fab fa-linkedin-in fa-2x"></i></a>
             </div>
-            <p href="bin/archived/index.html">nicholas-ionata (Git)</p>
+            <p href="bin/archived/index.html" class="footerSection">nicholas-ionata (Git)</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['data'],
+    props: ['data', 'navSelect'],
     data() {
         return {
+            mobileNavShow: false,
             navGroups: [
                 {
-                    icon: "fas fa-laptop-code",
+                    icon: "fa-laptop-code",
                     title: "WORKSPACE",
                     items: ["Life Status"]
                 },
                 {
-                    icon: "fas fa-code-branch",
+                    icon: "fa-code-branch",
                     title: "BRANCHES",
                     items: ["master", "experience", "projects"]
                 },
                 {
-                    icon: "fas fa-tag",
+                    icon: "fa-tag",
                     title: "TAGS",
                     items: ["languages", "tools", "technologies"]
                 },
                 {
-                    icon: "fas fa-cloud",
+                    icon: "fa-tag",
                     title: "REMOTES",
                     items: ["origin", "legacy"]
                 }
             ]
+        }
+    },
+    methods: {
+        onNavItemClick: function(item) {
+            this.$emit('navSelect', item)
+            this.mobileNavShow = false
+        },
+        onMobileNavToggle: function() {
+            this.mobileNavShow = !this.mobileNavShow
         }
     }
 }
