1187f924c1d72c54c6c449a80498c48c40704cfd
nionata <n.ionata129@gmail.com>
2019-07-17 23:05:48 -0400

    added nav group

--- src/components/controllers/SideNav.vue
+++ src/components/controllers/SideNav.vue
@@ -1,5 +1,14 @@
 <template>
     <div id="sideNav">
+        <div class="navGroup">
+            <div class="navMediaHolder">
+                
+            </div>
+            <div class="navContentHolder">
+                <p class="navTitle">WORKSPACE</p>
+                <p class="navItem">Life Status</p>
+            </div>
+        </div>
         <a href="bin/archived/index.html">legacy</a>
         <button v-on:click="$emit('navSelect', 'master')">master</button>
         <button v-on:click="$emit('navSelect', 'projects')">projects</button>
