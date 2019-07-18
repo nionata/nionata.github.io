abedf99c1a1be27c4863c1feadaa9bc72f07aba1
nionata <n.ionata129@gmail.com>
2019-07-17 22:22:23 -0400

    graph and modal are now "modules" we will see how long that lasts

---
+++ src/components/modules/Graph.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="graph">
+        <h2>My graph</h2>
+        <h1>{{ data.modal.display }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['data']
+}
+</script>
---
+++ src/components/modules/InformationModal.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="modal" v-bind:class="{modalEnabled: data.modal.enabled, modalDisabled: !data.modal.enabled}">
+        My modal
+        <button v-on:click="$emit('closeModal')">close</button>
+        {{data.modal.enabled}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['data']
+}
+</script>
