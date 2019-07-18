c3e38e75c2aebbeed247c7cafccce10bdc8c98e2
nionata <n.ionata129@gmail.com>
2019-07-17 22:22:46 -0400

    added props to sidenav and created main controller

---
+++ src/components/controllers/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="main">
+        <Graph v-bind:data="data"/>
+        <informationModal v-bind:data="data" v-on:closeModal="data.modal.enabled=false" />
+    </div>
+</template>
+
+<script>
+import Graph from '../modules/Graph.vue'
+import InformationModal from '../modules/InformationModal.vue'
+
+export default {
+    components: {Graph, InformationModal},
+    props: ['data']
+}
+</script>
--- src/components/controllers/SideNav.vue
+++ src/components/controllers/SideNav.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
+    <div id="sideNav">
         <a href="bin/archived/index.html">legacy</a>
+        <button v-on:click="$emit('navSelect', 'master')">master</button>
+        <button v-on:click="$emit('navSelect', 'projects')">projects</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: ['data']
 }
 </script>
