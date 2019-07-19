26fbdc6611d79bdff2da0c2a70ae684df8bd67d0
nionata <n.ionata129@gmail.com>
2019-07-19 09:01:21 -0400

    init graph

--- src/components/modules/Graph.vue
+++ src/components/modules/Graph.vue
@@ -1,12 +1,41 @@
 <template>
     <div id="graph">
-        <h2>My graph</h2>
-        <h1>{{ data.modal.display }}</h1>
+        <div id="branchContainer">
+        </div>
+        <div id="commitContainer">
+            <div 
+                class="commit"
+                v-for="commit in commits"
+                :key=commit.name
+                @click="onCommitClick(commit)"
+                :class="{selectedCommit: data.modal.selected.name===commit.name}"
+            >
+                <p class="commitName">{{commit.name}}</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import projects from '../../js/projects.json'
+import experience from '../../js/experience.json'
+
 export default {
-    props: ['data']
+    props: ['data'],
+    data() {
+        return {
+            commits: [
+                ...projects,
+                ...experience
+            ]
+        }
+    },
+    methods: {
+        onCommitClick: function(commit) {
+            this.data.modal.enabled = false
+            this.data.modal.selected = commit
+            this.data.modal.enabled = true
+        }
+    }
 }
 </script>
