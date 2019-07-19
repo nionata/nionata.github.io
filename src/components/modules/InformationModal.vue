bc1671ac0bfd7a890af4c7dfa564a1c3b12b339d
nionata <n.ionata129@gmail.com>
2019-07-19 09:01:28 -0400

    modal init

--- src/components/modules/InformationModal.vue
+++ src/components/modules/InformationModal.vue
@@ -1,13 +1,36 @@
 <template>
-    <div id="modal" v-bind:class="{modalEnabled: data.modal.enabled, modalDisabled: !data.modal.enabled}">
-        My modal
-        <button v-on:click="$emit('closeModal')">close</button>
-        {{data.modal.enabled}}
+    <transition name="custom-classes-transition"
+    enter-active-class="animated slideInLeft"
+    leave-active-class="animated slideOutLeft">
+    <div id="modal" v-if="data.modal.enabled" :class="{modalEnabled: data.modal.enabled, modalDisabled: !data.modal.enabled}">
+        <div id="modalLeft">
+            <div id="metaContainer">
+                hey
+            </div>
+            <div id="tagsContainer">
+                hi
+            </div>
+        </div>
+        <div id="modalRight">
+            <div id="modalDivider">
+            </div>
+            <div id="descriptionContainer">
+            </div>
+            <div id="toggleContainer">
+                <i class="fas fa-times" @click="onModalClose"/>
+            </div>
+        </div>
     </div>
+    </transition>
 </template>
 
 <script>
 export default {
-    props: ['data']
+    props: ['data'],
+    methods: {
+        onModalClose: function() {
+            this.data.modal.enabled = false
+        }
+    }
 }
 </script>
