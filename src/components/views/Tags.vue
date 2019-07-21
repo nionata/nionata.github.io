d23def3e5a0e945857db707746dd58ee18d033d6
nionata <n.ionata129@gmail.com>
2019-07-21 00:08:33 -0400

    init tags

---
+++ src/components/views/Tags.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="tags">
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    props: ['item'],
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
