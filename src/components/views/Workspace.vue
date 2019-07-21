53e1fc0d81f42cdcc1fd1bf604e0bbc0b721c057
nionata <n.ionata129@gmail.com>
2019-07-21 00:08:38 -0400

    init workspace

---
+++ src/components/views/Workspace.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="workspace">
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    props: ['item'],
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
