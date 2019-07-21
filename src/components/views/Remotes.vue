4ea10b893223a5ae2d2cbf023992958569ef2400
nionata <n.ionata129@gmail.com>
2019-07-21 00:49:51 -0400

    restyled log container and added remote container

--- src/components/views/Remotes.vue
+++ src/components/views/Remotes.vue
@@ -1,18 +1,30 @@
 <template>
     <div id="remotes">
-        <div id="originContainer" v-if="isItem('origin')">
-            <p id="title">Log</p>
-            <p class="remoteDescription">
-                2019-05: added some templating with Vue.js to my now <a href="/bin/archived/index.html" target="_blank">legacy</a> site <br/>
-                2019-06: "life is one big repo" <br/>
-                2019-07: started a full redesign, as the site was two and half years old, with the git repo idea <br/>
-                2019-08: published the current site and archived the original <br/>
-            </p>
-            <img id="designStages" src="/bin/design/phases/design-phases.gif" alt="design stages"/>
-            <p>The design phases</p>
+        <div class="remoteContainer" v-if="isItem('origin')">
+            <div class="logContainer">
+                <p id="title">Log</p>
+                <p class="remoteDescription">
+                    2019-03: added some templating with Vue.js to my now <a href="/bin/archived/index.html" target="_blank">legacy</a> site <br/>
+                    2019-06: "life is one big repo" <br/>
+                    2019-07: started a full redesign, as the site was two and half years old, with the git repo idea <br/>
+                    <img class="remoteImage" src="/bin/design/phases/design-phases.gif" alt="design stages"/> <br/>
+                    2019-08: published the current site and archived the original <br/>
+                </p>    
+            </div>
             <p>Origin powered by Vue.js, SASS, Webpack, and Babel</p>
         </div>
-        <div id="legacyContainer" v-if="isItem('legacy')">
+        <div class="remoteContainer" v-if="isItem('legacy')">
+            <div class="logContainer">
+                <p id="title">Log</p>
+                <p class="remoteDescription">
+                    2016-12: init <br/>
+                    <img class="remoteImage" id="legacyImage" src="/src/images/legacy.png" @click="onLegacyClick()" alt="legacy"/> <br/>
+                    2017-01: small updates <br/>
+                    2018-12: fixed broken link <br/>
+                    2019-03: added some templating with Vue.js to more easily add experiences and projects <br/>
+                </p>    
+            </div>
+            <p>Legacy powered by jQuery, Vue.js and Bootstrap</p>
         </div>
     </div>
 </template>
@@ -29,7 +41,9 @@ export default {
         isItem: function(item) {
             return item === this.item
         },
-
+        onLegacyClick: function() {
+            window.open("/bin/archived/index.html", "_blank")
+        }
     }
 }
 </script>
