a7dc3a342880505c070ae22cb95a60ab44f81c77
nionata <n.ionata129@gmail.com>
2019-07-30 11:04:26 -0400

    init commit org

--- src/components/views/Branches.vue
+++ src/components/views/Branches.vue
@@ -4,7 +4,7 @@
             <div id="branchContainer">
                 <div class="branch" v-for="commit in getCommits()" :key="commit.title">
                     <img 
-                        src="/src/images/branch.svg"
+                        src="/src/images/masterBranch.svg"
                         class="masterBranch"
                         v-if="commit.type !== 'master'"
                     />
@@ -36,19 +36,55 @@ import projectsRaw from '../../js/projects.json'
 import experienceRaw from '../../js/experience.json'
 import InformationModal from '../modules/InformationModal.vue'
 
+const tit = 'fasd'
+
+const initCommit = (year) => {
+    return {
+        "title": `init ${year}`,
+        "subTitle": "The start of a new year!",
+        "tags": ["Stuff!"],
+        "links": [],
+        "image": "",
+        "description": [],
+        "date": {
+            "start": "",
+            "end": ""
+        },
+        "type": "masterBranch",
+        "class": "masterBranch"
+    }
+}
+
+const mergeCommit = (year, type) => {
+    return {
+        "title": `merge '${type}' at the end of ${year}`,
+        "subTitle": `a good year of ${type}`,
+        "tags": ["Stuff!"],
+        "links": [],
+        "image": "",
+        "description": [],
+        "date": {
+            "start": "",
+            "end": ""
+        },
+        "type": "master",
+        "class": "masterBranch"
+    }
+}
+
 export default {
     components: {InformationModal},
     props: ['modal', 'item'],
     data() {
         return {
-            commitGroups: {}
+            commits: []
         }
     },
     methods: {
         getCommits: function() {
             switch(this.item) {
                 case 'master':
-                    return this.labelCommits([...projectsRaw, ...experienceRaw], 'master')
+                    return this.commits.merged
                 case 'experience':
                     return this.labelCommits(experienceRaw, 'experience')
                 case 'projects':
@@ -69,27 +105,43 @@ export default {
         onModalClose: function() {
             this.modal.enabled = false
             this.modal.selected = ''
+        },
+        processCommit: function(commitGroups, merged, meta) {
+            if(!commitGroups.length) return {merged, meta}
+            
+            const [year, commits] = commitGroups.shift()
+
+            meta[year] = {
+                projects: commits.find(commit => commit.type === 'projects') !== undefined,
+                experience: commits.find(commit => commit.type === 'experience') !== undefined,
+            }
+
+            merged.unshift(...commits)
+            if (meta[year].projects && commitGroups.length) merged.unshift(mergeCommit(year, 'projects'))
+            if (meta[year].experience && commitGroups.length) merged.unshift(mergeCommit(year, 'experience'))
+
+            return this.processCommit(commitGroups, merged, meta)
         }
     },
     mounted: function() {
         const projects = this.labelCommits(projectsRaw, 'projects')
         const experience = this.labelCommits(experienceRaw, 'experience')
-
+        
         let commitGroups = {}
         let commits = [...projects, ...experience]
 
-        commits.sort((a, b) => new Date(a.date.start) - new Date(b.date.start))
+        commits.sort((a, b) => new Date(a.date.start) - new Date(b.date.start))        
         commits.forEach(commit => {
             const year = new Date(commit.date.start).getFullYear()
 
             if (commitGroups[year]) {
-                commitGroups[year].push(commit)
+                commitGroups[year].unshift(commit)
             } else {
-                commitGroups[year] = [commit]
+                commitGroups[year] = [commit, initCommit(year)]
             }
         })
-    
-        this.commitGroups = commitGroups
+
+        this.commits = this.processCommit(Object.entries(commitGroups), [], {})
     }
 }
 </script>
