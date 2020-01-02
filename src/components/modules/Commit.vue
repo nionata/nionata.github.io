6e3e339dc2c1a339e2bff8456706586b0adfd9b0
nionata <n.ionata129@gmail.com>
2020-01-02 15:12:59 -0500

    implemented tags, but commented out for now

--- src/components/modules/Commit.vue
+++ src/components/modules/Commit.vue
@@ -7,7 +7,7 @@
             <img v-for="branch in branches" :src="branch.img" :class="branch.class" :key="branch.class" />
         </div>
         <div class="messageContainer" :class="{titleMessage: commit.type !== 'master', selectedCommit: selected}" @click="$emit('onClick', commit)" :key="commit.title">
-            <!-- <p class="tag" v-if="shouldTag(index)"><i class="fas fa-code-branch" />{{commit.type}}</p> -->
+            <!--<p class="tag" v-if="tag"><i class="fas fa-code-branch" />{{commit.type}}</p>-->
             <p class="commitTitle" v-if="commit.type !== 'master'">{{commit.title}}</p>
         </div>
     </div>
