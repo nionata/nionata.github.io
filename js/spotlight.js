09389657453c0380f8085acd6bcd17cb3d1e22ad
nionata <n.ionata129@gmail.com>
2020-05-01 11:13:22 -0400

    organized index and added spotlight

---
+++ js/spotlight.js
@@ -0,0 +1,26 @@
+var spotlight = [
+  {
+    name: "visa",
+    link: "https://cdn.visa.com/cdn/assets/images/logos/visa/logo.png"
+  },
+  {
+    name: "streammonkey",
+    link: "https://static.streammonkey.com/dist/images/logo.svg"
+  },
+  {
+    name: "raymondjames",
+    link: "https://stpeteedc.com/wp-content/uploads/2018/03/raymond-james.png"
+  },
+  {
+    name: "lockheedmartin",
+    link: "https://gateway.foresee.com/sites/lockheed-martin/production/trigger/LM_mobile.jpg"
+  },
+  {
+    name: "swamphacks",
+    link: "https://2019.swamphacks.com/assets/mainpage/white_logo.png"
+  },
+  {
+    name: "scanned",
+    link: "https://nicholasionata.com/Scanned-Website/images/logo.png"
+  }
+]
