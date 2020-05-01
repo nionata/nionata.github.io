6e1de7ee49f0a6691a544d0653962501d54a7a1e
nionata <n.ionata129@gmail.com>
2020-05-01 18:16:18 -0400

    migrated splash to vue, updated fa, and removed animate

--- js/vue.js
+++ js/vue.js
@@ -1,6 +1,46 @@
 const timeout = 2500
 
 function initVue() {
+  Vue.component('splash', {
+    props: ['dev'],
+    template: `
+      <div class="section-col primary-color" id="splash">
+          <div class="section-item section-row" id="splash-header">
+            <img src="images/me.png" id="splash-img" 
+                @mouseover="dev.show=true"
+            />
+          </div>
+          <div class="section-item" id="splash-about">
+            <h1 id="splash-about-title">
+              Hi, I'm Nicholas Ionata
+            </h1>
+            <div id="splash-about-content">
+                <h3>
+                  I study 
+                  <span class="splash-about-highlight">
+                    Computer Science
+                  </span> 
+                  at the 
+                  <span class="splash-about-highlight">
+                    University of Florida
+                  </span>
+                </h3>
+                <h3>
+                  In my freetime I
+                  <span class="splash-about-highlight">
+                    make
+                  </span>
+                  cool things
+                </h3>
+            </div>
+          </div>
+          <a href="#experience">
+            <i class="fa fa-chevron-down fa-5x" aria-hidden="true" id="splash-action"/>
+          </a>
+      </div>
+    `
+  })
+
   Vue.component('spotlight', {
     props: ['links'],
     template: `
@@ -17,7 +57,11 @@ function initVue() {
     data: {
       spotlight: spotlight,
       projects: projects,
-      experience: experience
+      experience: experience,
+      dev: {
+        mode: false,
+        show: false
+      }
     },
     methods: {
       rotateSpotlight: function() {
