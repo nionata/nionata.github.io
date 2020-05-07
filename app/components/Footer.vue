84e2f58391a46efc8bd32dbdf4579653df0e06d8
nionata <n.ionata129@gmail.com>
2020-05-07 14:53:26 -0400

    styled footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="footer" class="col">
-		<div id="contactContainer" class="item row">
+		<div id="contactContainer" class="item rowNarrow">
 			<div class="item contact" v-for="contact in contacts" :key="contact.icon">
 				<a :href="contact.link" target="_blank">
 					<i class="fa fa-3x" :class="contact.icon" />
