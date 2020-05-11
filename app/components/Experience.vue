7fdc85e6727de9f97c2e334fd16a0b35391974fd
nionata <n.ionata129@gmail.com>
2020-05-10 20:46:50 -0400

    use vue transition to dynamically render experiences

--- app/components/Experience.vue
+++ app/components/Experience.vue
@@ -4,7 +4,7 @@
       <Header />
       <h2 class="sectionTitle">Experience</h2>
     </div>
-    <div class="experience" v-for="exp in experiences" :key="exp.title">
+    <div class="experience" v-for="(exp, x) in experiences" :key="exp.title">
       <div class="dateContainer">
           <p v-if="exp.type === 'date'" :class="exp.type">{{exp.title}}</p>
       </div>
@@ -16,7 +16,7 @@
           :type="type"
         />
       </div>
-      <div class="detailsContainer" v-if="exp.type !== 'date'">
+      <div class="detailsContainer" v-if="exp.type !== 'date'" @mouseenter="active = x" @mouseleave="active = -1">
          <div id="detailsHeader" class="rowBare" :class="exp.type">
             <div class="rowBare">
               <p v-if="exp.type === 'work'">{{exp.subTitle}} @&nbsp</p>
@@ -26,7 +26,8 @@
               <span class="outline" v-for="(tag, i) in exp.tags" :key="i">{{tag}}</span>
             </div>
          </div>
-         <div id="detailsBody" class="colBare">
+         <transition name="fade">
+         <div id="detailsBody" class="colBare" v-if="active == x">
            <div class="rowBare item">
               <p>{{exp.date.start}}</p>
               <p v-if="exp.date.end">&nbsp- {{exp.date.end}}</p>
@@ -38,6 +39,7 @@
               <a v-for="(link, i) in exp.links" :href="link.link" target="_blank" :key="i"><span>{{link.type}}</span></a>
             </div>
          </div>
+         </transition>
       </div>
     </div>
   </div>
