55dc2e7f51352dee1aa20106c407e05890833940
Nick Ionata <nionata@ufl.edu>
2020-12-30 14:16:24 -0500

    make list items show optional - true by default; handle click on list items

--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -2,14 +2,14 @@ import ListItem, { Props as ListItemProps } from '@components/ListItem'
 
 export type Props = {
     items: Array<ListItemProps>,
-    show: boolean,
+    show?: boolean,
     click?(): void
 }
 
-const ListItems = ({ items, show, click = null }: Props) => {
+const ListItems = ({ items, show = true, click = null }: Props) => {
     return (
         <div className={`${show ? '' : 'invisible'} my-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5`}
-            onClick={() => click()}
+            onClick={click ? () => click() : click}
         >
             <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                 {items.map(item => <ListItem {...item} />)}
