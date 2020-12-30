a093d8033acc9f62b4293972b3ca828e9fb31697
Nick Ionata <nionata@ufl.edu>
2020-12-29 19:20:40 -0500

    add the onclick to list item

--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -9,7 +9,7 @@ export type Props = {
 const ListItems = ({ items, show, click = null }: Props) => {
     return (
         <div className={`${show ? '' : 'invisible'} my-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5`}
-            onClick={() => console.log('yuo')}
+            onClick={() => click()}
         >
             <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                 {items.map(item => <ListItem {...item} />)}
