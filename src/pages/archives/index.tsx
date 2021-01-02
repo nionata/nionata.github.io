8f0a34371e751b5bea0fee9a05bf02b389037c14
Nick Ionata <nionata@ufl.edu>
2021-01-02 16:34:18 -0500

    init archives page

--- src/pages/archives/index.tsx
+++ src/pages/archives/index.tsx
@@ -1,25 +1,39 @@
 import { GetStaticProps } from "next"
 import * as Archives from '@utils/archives'
 import ListItems, { Props as ListItemsProps } from "@components/ListItems"
+import ListItem from "@components/ListItem"
 
 type Props = {
     versions: ListItemsProps['items']
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }): Promise<{ props: Props }> => 
-{
-    const versions = Archives.getItemizedVersions()
-    
+{   
     return {
-        props: { versions }
+        props: { 
+            versions: Archives.getItemizedVersions() 
+        }
     }
 }
 
 const ArchivesHome = ({ versions }: Props) => 
 {
     return (
-        <div>
-            <ListItems items={versions} />
+        <div className="flex flex-col items-center justify-center h-full">
+            <div className="w-1/2 text-center pb-2">
+                <h1 className="text-xl">Archives</h1>
+                <p>Since 2016, this site has served as a playground for testing new tech. These are the iterations.</p>
+            </div>
+
+            {/* <ListItem
+                key="yolo"
+                name="test"
+                link="/archives/v2.0"
+            /> */}
+
+            <div className="w-1/2 pt-2 border-t-2 border-blue-300">
+                <ListItems items={versions} />
+            </div>
         </div>
     )
 }
