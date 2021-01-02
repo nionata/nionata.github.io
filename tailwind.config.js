b84ec2bf00789ff5daaecb826e3112bb3f33caa3
Nick Ionata <nionata@ufl.edu>
2021-01-02 16:33:34 -0500

    add a tailwind flex center

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
 	purge: ['src/pages/**/*.tsx', 'src/components/**/*.tsx'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-	extend: {},
+		flex: {
+			'5': '5 5 0%'
+		},
 	},
 	variants: {
 	extend: {},
