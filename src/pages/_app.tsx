d4131740080207a06049257a9974d24ddf2b4f4e
Nick Ionata <nionata@ufl.edu>
2021-01-02 16:34:36 -0500

    init global navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import '@styles/globals.css'
 import { AppProps } from 'next/app'
+import Image from 'next/image'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    // <div>
-      <Component {...pageProps} />
-    // </div>
-  )
+	return (
+		<div className="h-screen">
+			<div className="h-1/12 bg-blue-200 flex items-center">
+				<Image 
+					src="/logo.png"
+					width={100}
+					height={100}
+					layout="fixed"
+				/>
+			</div>
+			<div className="">
+				<Component {...pageProps} />
+			</div>
+		</div>
+	)
 }
